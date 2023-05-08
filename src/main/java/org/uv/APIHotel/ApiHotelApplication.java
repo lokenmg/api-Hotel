373c87365428a7a1a69e790180a6f93bee202cc3
loken <rodrigomencias08@gmail.com>
2023-05-07 19:06:34 -0600

    Creación de API base

---
+++ src/main/java/org/uv/APIHotel/ApiHotelApplication.java
@@ -0,0 +1,13 @@
+package org.uv.APIHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/uv/APIHotel/ApiHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package org.uv.APIHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
